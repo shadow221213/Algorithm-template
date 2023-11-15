f720e67666c10591c17a9fec554f15e698118184
shadow221213 <1069493541@qq.com>
2023-11-15 12:23:57 +0800

    fix recursion

--- 其他/函数内递归.cpp
+++ 其他/函数内递归.cpp
@@ -1,6 +1,18 @@
-auto dfs = [&] ( ) {
-    auto calc = [&] (auto &&self) {
-        // ...
+/*
+ * @Description:
+ * @Author: shadow221213
+ * @Date: 2023-11-09 15:16:40
+ * @LastEditTime: 2023-11-15 12:23:03
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main( ) {
+    auto dfs = [&] ( ) -> int {
+        auto calc = [&] (auto &&self) -> int {
+            // ...
+            return self(self);
+            };
+        return calc(calc);
         };
-    return calc(calc);
-    };
+}
